77b6c0d8f70a7930eefbe5738c20be01f5ac89aa
seros <github@seros.me>
2026-07-15 11:16:13 +0800

    1

---
+++ me-platform/me-gateway/src/main/java/me/filter/CacheBodyFilter.java
@@ -0,0 +1,24 @@
+package me.filter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.event.EnableBodyCachingEvent;
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CacheBodyFilter {
+    private final RouteLocator routeLocator;
+    private final ApplicationEventPublisher publisher;
+
+    @EventListener(RefreshRoutesEvent.class)
+    public void onRoutesRefreshed() {
+        routeLocator.getRoutes().subscribe(routeDefinition -> {
+            EnableBodyCachingEvent event = new EnableBodyCachingEvent(new Object(), routeDefinition.getId());
+            publisher.publishEvent(event);
+        });
+    }
+}
