cfdbee07e46b968674c5113eed1f36e40ce8b228
seros <github@seros.me>
2026-07-15 10:39:34 +0800

    1

---
+++ src/main/java/me/MeCloudApplication.java
@@ -0,0 +1,11 @@
+package me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeCloudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MeCloudApplication.class, args);
+    }
+}
---
+++ src/test/java/me/MeCloudApplicationTests.java
@@ -0,0 +1,11 @@
+package me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeCloudApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
