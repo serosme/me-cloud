8490127a95ce6fc95f4c7f71f36a88a6e7d0f42f
seros <github@seros.me>
2026-07-29 14:43:14 +0800

    1

---
+++ example-platform/example-platform-system/src/main/java/com/example/platform/system/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.example.platform.system.mapper;
+
+import com.example.platform.system.entity.User;
+import com.github.yulichang.base.MPJBaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends MPJBaseMapper<User> {
+}
